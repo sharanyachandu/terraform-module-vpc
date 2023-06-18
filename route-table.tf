ca3b5ee43054494a0fcb10b03ca51614465b2d61
sharanya chandu <you@example.com>
2023-06-18 23:21:05 +0530

    auto save

---
+++ route-table.tf
@@ -0,0 +1,18 @@
+# Creates Public route Table
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
+}
