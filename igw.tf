6294bcb865b46958a2804ae1b7951d7124464a1e
sharanya chandu <you@example.com>
2023-06-19 10:33:26 +0530

    auto save

--- igw.tf
+++ igw.tf
@@ -6,3 +6,24 @@ resource "aws_internet_gateway" "igw" {
     Name = "roboshop-${var.ENV}-igw"
   }
 }
+#Creating Elastic IP for the aws_nat_gateway
+resource "aws_eip" "ngw-eip" {
+ vpc    =  true
+ tags ={
+    Name = "robot-${var.ENV}-ngw-eip"
+ }
+}
+
+#Created NAT gatway to expose the public network to private machines
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "robot-${var.ENV}-ngw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_eip.ngw-eip]
+}
--- route-table.tf
+++ route-table.tf
@@ -23,3 +23,29 @@ resource "aws_route_table_association" "public-rt-association" {
     subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
     route_table_id = aws_route_table.public-rt.id
 }  
+
+# Creates private route Table
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.ngw.id
+  }
+
+  route {
+    cidr_block                 = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id  = aws_vpc_peering_connection.peer.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-rt"
+  }
+}
+
+#Attach the private rt to the private Subnets
+resource "aws_route_table_association" "private-rt-association" {
+    count          = length(aws_subnet.privatec_subnet.*.id)
+    subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+    route_table_id = aws_route_table.private-rt.id
+}  
