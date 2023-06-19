53bbb943140ab6a16cb144013ebbf70c6eaa6292
sharanya chandu <you@example.com>
2023-06-19 11:23:09 +0530

    auto save

--- route-table.tf
+++ route-table.tf
@@ -17,20 +17,21 @@ resource "aws_route_table" "public-rt" {
   }
 }
 
-#Attach the public rt to the public Subnets
+# Attaches the public rt to the public subnets
 resource "aws_route_table_association" "public-rt-association" {
-    count          = length(aws_subnet.public_subnet.*.id)
-    subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
-    route_table_id = aws_route_table.public-rt.id
-}  
+  count          = length(aws_subnet.public_subnet.*.id)
 
-# Creates private route Table
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# # Creates Private route Table
 resource "aws_route_table" "private-rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id           = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.ngw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
@@ -43,9 +44,19 @@ resource "aws_route_table" "private-rt" {
   }
 }
 
-#Attach the private rt to the private Subnets
+# Attaches the private rt to the private subnets
 resource "aws_route_table_association" "private-rt-association" {
-    count          = length(aws_subnet.privatec_subnet.*.id)
-    subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-    route_table_id = aws_route_table.private-rt.id
-}  
+  count          = length(aws_subnet.private_subnet.*.id)
+
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private-rt.id
+}
+
+
+# Adding the peering route entry insde the default route-table to access robot network
+resource "aws_route" "def-vpc-robot-vpc-root" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  depends_on                = [aws_vpc_peering_connection.peer]
+}
